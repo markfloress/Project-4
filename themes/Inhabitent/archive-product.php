b538102dd814ba9a6011c0a648489c8d58f9f63f
Mark <mark@Marks-MacBook-Pro.local>
2017-08-17 11:25:58 -0700

    working on the product page

--- themes/Inhabitent/archive-product.php
+++ themes/Inhabitent/archive-product.php
@@ -14,10 +14,16 @@ get_header(); ?>
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					// the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+      </header><!-- .page-header -->
+      
+      <div>
+
+
+
+
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
