4d87ca9b462e86d2b8f08a0590f774e1243e878f
Mark <mark@Marks-MacBook-Pro.local>
2017-08-30 15:36:50 -0700

    added search toggle and compiled the new JS file

--- themes/Inhabitent/functions.php
+++ themes/Inhabitent/functions.php
@@ -92,6 +92,7 @@ function red_starter_scripts() {
 	wp_enqueue_script('jquery');
 	
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'red-starter-main', get_template_directory_uri() . '/build/js/main.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ themes/Inhabitent/js/main.js
@@ -0,0 +1,14 @@
+(function( $ ) {
+  $('.search-toggle').on('click', function(){
+      $('.search-field').toggleClass('fast');
+      $( '.search-field' ).focus();
+      return false;
+  });
+ 
+ $(document).on('click', function(event){
+      if (!$('.search-field').is( event.target )){
+         $( ".search-field" ).hide('fast');
+      }
+  });
+ 
+ })( jQuery );
